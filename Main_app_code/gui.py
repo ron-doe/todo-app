b83c1f07f9288039944284f11a4dde8b2c2b791b
ronald-leon <ronald.leon@ibm.com>
2025-06-30 22:31:54 -0600

    not really a change, just changing my computer and making sure I have it all saved

--- Main_app_code/gui.py
+++ Main_app_code/gui.py
@@ -1,6 +1,11 @@
 from modules import functions
 import FreeSimpleGUI as sg
 import time
+import os
+
+if not os.path.exists("to_dos.txt"):
+    with open("to_dos.txt", "w") as file:
+        pass
 
 sg.theme('Black')
 
